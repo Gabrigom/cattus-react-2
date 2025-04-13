fa3087af63c1619cc5481c727e1655da100b49b9
Gabrigom <vinny.gab@gmail.com>
2025-04-13 16:26:27 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from "@/Components/ui/button"
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
